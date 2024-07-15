253a9414eb878578b6e19f4837cb23b7d7d4efef
maicra2505 <patatatriste736@gmail.com>
2024-07-15 16:49:42 +0000

    Mundo guardado 2024-07-15

--- server.py
+++ server.py
@@ -33,6 +33,7 @@ def download_latest_release(download_path='.'):
 		data = pet.json()
 		url = data.get('latest')
 		version = url.split("/")[-1]
+		
 		if version in glob.glob("*.msp"):
 			return version
 		else:
